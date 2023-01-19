5936c9f629f6c75e1fd88dd197e0aa3cfb64cc05
Luigi Pesaro <113304963+lpesaro@users.noreply.github.com>
2023-01-19 18:46:40 +0100

    Add files via upload

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpesaro <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 22:44:40 by lpesaro           #+#    #+#             */
+/*   Updated: 2022/09/27 19:52:35 by lpesaro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	int	i;
+
+	i = nb;
+	if (power < 0)
+	{
+		return (0);
+	}
+	else if (power == 0)
+	{
+		return (1);
+	}
+	else if (power > 0)
+	{
+		return (nb * ft_recursive_power(nb, power - 1));
+	}
+	return (nb);
+}
